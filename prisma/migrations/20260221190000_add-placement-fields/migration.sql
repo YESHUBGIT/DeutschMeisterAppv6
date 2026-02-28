b5f3ae08c2d4449b48057f2112e3bf60d7d4628d
yeshram <yeshram@example.com>
2026-02-28 16:57:17 +0100

    version 6 sudo error

--- prisma/migrations/20260221190000_add-placement-fields/migration.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "UserProgress" ADD COLUMN "placementLevel" TEXT;
-ALTER TABLE "UserProgress" ADD COLUMN "placementSource" TEXT;
-ALTER TABLE "UserProgress" ADD COLUMN "placementScore" INTEGER;
-ALTER TABLE "UserProgress" ADD COLUMN "placementCompletedAt" TIMESTAMP(3);
